2f8138efaa396e0b38bd176264ebbf05c2b0efc0
Valliammai S <valliammai.subbu@gmail.com>
2020-06-27 19:01:19 -0400

    project view module changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {UserAddComponent} from 'src/app/view/user-add/user-add.component';
 import {UserViewComponent} from 'src/app/view/user-view/user-view.component';
+import {ProjectAddComponent} from 'src/app/view/project-add/project-add.component';
+import {ProjectViewComponent} from 'src/app/view/project-view/project-view.component';
 
 
 const routes: Routes = [
-  {path:'userAdd',component:UserAddComponent},
-  {path:'userView',component:UserViewComponent}
+  {path:'useradd',component:UserAddComponent},
+  {path:'userview',component:UserViewComponent},
+  {path:'projectadd',component:ProjectAddComponent},
+  {path:'projectview',component:ProjectViewComponent}
 ];
 
 @NgModule({
@@ -14,33 +18,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-/*import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {AddProjectAddComponent} from 'src/app/UI/add-project/add-project.component';
-import {AddTaskComponent} from 'src/app/UI/add-task/add-task.component';
-import {AddUserComponent} from 'src/app/UI/add-user/add-user.component';
-import {UpdateProjectComponent} from 'src/app/UI/update-project/update-project.component';
-import {UpdateTaskComponent} from 'src/app/UI/update-task/update-task.component';
-import {UpdateUserComponent} from 'src/app/UI/update-user/update-user.component';
-import {ViewProjectComponent} from 'src/app/UI/view-project/view-project.component';
-import {ViewTaskComponent} from 'src/app/UI/view-task/view-task.component';
-import {ViewUserComponent} from 'src/app/UI/view-user/view-user.component';
-
-
-const MAINMENU_ROUTES: Routes = [
-    { path: '', redirectTo: '/add-project', pathMatch: 'full' },
-    {path:'add-task',component:AddTaskComponent},
-    {path:'update-task',component:UpdateTaskComponent},
-    {path:'view-task',component:ViewTaskComponent},
-    {path:'add-project',component:AddProjectComponent},
-    {path:'update-project',component:UpdateProjectComponent},
-    {path:'view-project',component:ViewProjectComponent},
-    {path:'add-user',component:AddUserComponent},
-    {path:'update-user',component:UpdateUserComponent},
-    {path:'view-user',component:ViewUserComponent}
-
-  ];
-
-  export const CONST_ROUTING = RouterModule.forRoot(MAINMENU_ROUTES);
-*/
--- src/app/view/project-view/project-view.component.ts
+++ src/app/view/project-view/project-view.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+
+import { SharedService } from 'src/app/service/shared.service';
+import { DatePipe } from '@angular/common';
+import { Project } from 'src/app/model/project';
+import { Router } from '@angular/router';
+
+import { ModalPopupComponent } from 'src/app/view/modal-popup/modal-popup.component';
 
 @Component({
   selector: 'app-project-view',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectViewComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(ModalPopupComponent,{static:false}) modalPopup: ModalPopupComponent;
+
+  projList:Project[];
+  SearchProjId:number;
+  selectedProject: any;
+  public searchTerm:string;
+
+  showModal: boolean;
+
+  constructor(private service:SharedService,
+              private router:Router,
+              private datePipe: DatePipe) {
+    this.service.GetAllProject().subscribe(x=> this.projList = x);
+   }
 
   ngOnInit() {
+  }
+
+  SearchProjByID(id){
+    this.service.GetProjectById(this.SearchProjId).subscribe(k=>this.projList=k);
+  }
+
+  btnUpdateClick(id)
+  {
+   this.router.navigate(["projectupdate"],{queryParams:{ProjectID:id}});
+  }
+
+  btnSuspendClick(projId)
+  {
+    this.selectedProject = this.projList.find(proj => proj.ProjectID === projId);
+    this.selectedProject.EndDate = this.datePipe.transform(new Date(), "yyyy-MM-dd");
+    this.selectedProject.Status = 'Suspended';
+
+
+    this.service.UpdateProject(projId, this.selectedProject)
+      .subscribe(
+        data => {
+          this.modalPopup.show();
+          this.showModal = true;
+          this.modalPopup.setContent("Project Has Been Suspended Successfully.!" , "Project");
+        },
+        error => {
+          this.modalPopup.setContent("Request Failed!" , "");
+        });
   }
 
 }
